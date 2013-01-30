16f78da5668e39a09cf193fb0e643e0cab6f184f
Matthew Ueckerman <matthew@ueckerman.net>
2013-01-31 09:07:56 +1100

    Corrected Selenium driver override support; driver type converted to symbol

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -2,7 +2,8 @@ Capybara.run_server = false
 Capybara.app_host = "http://localhost:3000"
 
 Capybara.register_driver(:selenium_override) do |app|
-  AccountRightMobile::Acceptance::Drivers::DriverFactory.create(type: ENV['driver'] || :firefox, app: app)
+  type = (ENV['driver'] &&  ENV['driver'].to_sym) || :firefox
+  AccountRightMobile::Acceptance::Drivers::DriverFactory.create(type: type, app: app)
 end
 
 Capybara.default_driver = :selenium_override #Defaults to :rack_test
