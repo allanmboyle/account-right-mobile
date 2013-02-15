16793df951c765e547de89120315ce17c29b558e
Matthew Ueckerman <matthew@ueckerman.net>
2013-02-15 13:21:06 +1100

    Rails server process is managed by pid file created by Rails

--- lib/account_right_mobile/rails_server.rb
+++ lib/account_right_mobile/rails_server.rb
@@ -6,11 +6,18 @@ module AccountRightMobile
     def initialize(options)
       super(options.merge(name: "rails_#{options[:environment]}_server"))
       @environment = options[:environment]
-      @deletable_artefacts << DEFAULT_PID_FILE_PATH
     end
 
     def start_command
       "rails s -e #{@environment} -p #{@port}"
+    end
+
+    def pid_file_path
+      DEFAULT_PID_FILE_PATH
+    end
+
+    def create_pid_file(pid)
+      # Intentionally blank as Rails creates pid file
     end
 
   end
