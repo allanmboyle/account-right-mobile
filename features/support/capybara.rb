a4ad0c59b5c2449d27c249d23ad3e5dc280c3b7c
Matthew Ueckerman <matthew@ueckerman.net>
2013-03-19 00:24:49 +1100

    Acceptance timeout configured by environment

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,11 +1,13 @@
+configuration = AccountRightMobile::Application.config.acceptance
+
 Capybara.run_server = false
-Capybara.app_host = AccountRightMobile::Application.config.acceptance["app_uri"]
+Capybara.app_host = configuration["app_uri"]
 
 Capybara.register_driver(:selenium_override) do |app|
   type = (ENV["driver"] &&  ENV["driver"].to_sym) || :iphone_firefox
   AccountRightMobile::Acceptance::Drivers::DriverFactory.create(type: type, app: app)
 end
 
-Capybara.default_wait_time = AccountRightMobile::Wait.default_timeout_in_seconds = 3 # seconds
+Capybara.default_wait_time = AccountRightMobile::Wait.default_timeout_in_seconds = configuration["timeout_in_seconds"]
 Capybara.default_driver = :selenium_override #Defaults to :rack_test
 Capybara.javascript_driver = :selenium_override #Defaults to :selenium
