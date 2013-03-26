772f490490d6f08801b964e95e670d502f8f1f99
Matthew Ueckerman <matthew@ueckerman.net>
2013-03-26 12:45:44 +1100

    Session expires after 20 minutes of inactivity

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-AccountRightMobile::Application.config.session_store :cookie_store, :key => '_account-right-mobile_session'
+AccountRightMobile::Application.config.session_store :cookie_store, :key => '_account-right-mobile_session',
+                                                                    :expire_after => 20.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
