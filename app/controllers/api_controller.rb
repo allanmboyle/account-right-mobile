65494e8ddd0f972119df0e8e33f5ea4861732372
Matthew Ueckerman <matthew@ueckerman.net>
2013-04-03 11:41:06 +1100

    Simplified API controllers delegation to API model

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,17 +2,9 @@ class ApiController < ApplicationController
 
   def invoke
     respond_to_json do
-      response = AccountRight::API.invoke(request[:resource_path], security_tokens)
+      response = AccountRight::API.invoke(request[:resource_path], session)
       render :json => response
     end
-  end
-
-  private
-
-  def security_tokens
-    security_tokens = { access_token: session[:access_token] }
-    security_tokens[:cf_token] = session[:cf_token] if session[:cf_token]
-    security_tokens
   end
 
 end
--- spec/app/controllers/api_controller_spec.rb
+++ spec/app/controllers/api_controller_spec.rb
@@ -2,57 +2,49 @@ describe ApiController, type: :controller do
 
   describe "#invoke" do
 
-    let(:access_token) { "some_access_token" }
-    let(:resource_path) { "some/resource/path" }
+    describe "when a json request is made" do
 
-    before(:each) { session[:access_token] = access_token }
+      let(:access_token) { "some_access_token" }
+      let(:resource_path) { "some/resource/path" }
 
-    describe "when a customer file token has been established in the users session" do
+      before(:each) { session[:access_token] = access_token }
 
-      let(:cf_token) { "some_customer_file_token" }
-
-      before(:each) { session[:cf_token] = cf_token }
-
-      it "should also invoke the API with the users customer file token" do
-        AccountRight::API.should_receive(:invoke).with(resource_path, access_token: access_token, cf_token: cf_token)
+      it "should invoke the API with the users session which contains security tokens" do
+        AccountRight::API.should_receive(:invoke).with(resource_path, session)
 
         get_invoke
+      end
+
+      describe "when the API responds with data" do
+
+        before(:each) do
+          AccountRight::API.stub!(:invoke).and_return("some json")
+        end
+
+        it "should respond with status of 200" do
+          get_invoke
+
+          response.status.should eql(200)
+        end
+
+        it "should return the response from the api" do
+          get_invoke
+
+          response.body.should eql("some json")
+        end
+
+      end
+
+      def get_invoke
+        request_action resource_path: resource_path, format: :json
       end
 
     end
 
-    describe "when a customer file token has not been established in the users session" do
+    it_should_behave_like "an action that only accepts json"
 
-      it "should invoke the API requesting the provided resource path with the users access token" do
-        AccountRight::API.should_receive(:invoke).with(resource_path, access_token: access_token)
-
-        get_invoke
-      end
-
-    end
-
-    describe "when the API responds with data" do
-
-      before(:each) do
-        AccountRight::API.stub!(:invoke).and_return("some json")
-      end
-
-      it "should respond with status of 200" do
-        get_invoke
-
-        response.status.should eql(200)
-      end
-
-      it "should return the response from the api" do
-        get_invoke
-
-        response.body.should eql("some json")
-      end
-
-    end
-
-    def get_invoke
-      get :invoke, resource_path: resource_path, format: :json
+    def request_action(options)
+      get :invoke, options
     end
 
   end
